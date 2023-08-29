7bb2fb8718d2303fde120433c522e2945f39eacb
Aki9190 <0mj326621w6613@gmail.com>
2023-08-30 07:33:36 +0900

    適切なコミットメント

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,13 +9,13 @@
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach($posts as $post)
+            @foreach ($posts as $post)
                 <div class='post'>
-                    <a href= "/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+                    <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>{{ $posts->links()}}</div>
+        <div class="paginate">{{ $posts->links()}}</div>
     </body>
 </html>
